44e8bb2005e2c27793f88b7d7ae1ac7d84208f32
Alexander Mann <amann@c1r4p7.hive.fi>
2022-06-03 12:54:18 +0300

    testing new remote

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amann <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:48:52 by amann             #+#    #+#             */
-/*   Updated: 2022/06/02 13:09:24 by amann            ###   ########.fr       */
+/*   Updated: 2022/06/03 12:54:02 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <limits.h>
 
+/* testing */
 int	main(void)
 {
 	//testing getcwd and chdir
